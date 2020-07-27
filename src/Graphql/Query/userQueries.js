7e013205a58db71dc985874dadf7624a02528b08
Enrique Devars <e.devars.dev@gmail.com>
2020-07-27 00:11:11 -0500

    :sparkles: Creating query to get users

--- src/Graphql/Query/userQueries.js
+++ src/Graphql/Query/userQueries.js
@@ -1,13 +1,25 @@
+const { madeQuery } = require('../../lib/db')
+
 const getUser = () => {
     return {
         user_id: 1,
-        name: 'Enrique',
+        user_name: 'Enrique',
         user_url_photo: 'photo.png',
         email: 'enrique.devars@gmail.com',
         password: 'some shit'
     }
 }
 
+const getUsers = async () => {
+    try {
+        const queryResults = await madeQuery('select * from users')
+        return queryResults
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 module.exports = {
-    getUser
+    getUser,
+    getUsers
 }
--- src/Graphql/typeDefs.js
+++ src/Graphql/typeDefs.js
@@ -3,14 +3,15 @@ const { gql } = require('apollo-server')
 const typeDefs = gql`
     type User {
         user_id: ID!
-        name: String!
+        user_name: String!
         user_url_photo: String!
         email: String!
         password: String!
     }
 
     type Query {
-        getUser: User
+        getUser: User,
+        getUsers: [User]
     }
 `
 
